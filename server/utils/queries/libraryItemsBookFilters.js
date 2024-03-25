1b2f7a30bf9b4164a0f4fd5749eca7ee2c065cad
Lauri Vuorela <lauri.vuorela@gmail.com>
2024-03-25 12:20:21 +0100

    fixes

--- server/utils/queries/libraryItemsBookFilters.js
+++ server/utils/queries/libraryItemsBookFilters.js
@@ -658,9 +658,11 @@ module.exports = {
     let includeAttributes = [
       [Sequelize.literal('(SELECT max(mp.updatedAt) FROM bookSeries bs, mediaProgresses mp WHERE mp.mediaItemId = bs.bookId AND mp.userId = :userId AND bs.seriesId = series.id)'), 'recent_progress'],
     ]
-    let booksNotFinishedQuery = ``
+    let booksNotFinishedQuery = `(SELECT count(*) FROM bookSeries bs LEFT OUTER JOIN mediaProgresses mp ON mp.mediaItemId = bs.bookId AND mp.userId = :userId WHERE bs.seriesId = series.id AND (mp.isFinished = 0 OR mp.isFinished IS NULL))`
+
     if (library.settings.onlyShowLaterBooksInContinueSeries) {
       includeAttributes.push([Sequelize.literal('(SELECT CAST(max(bs.sequence) as FLOAT) FROM bookSeries bs, mediaProgresses mp WHERE mp.mediaItemId = bs.bookId AND mp.isFinished = 1 AND mp.userId = :userId AND bs.seriesId = series.id)'), 'maxSequence'])
+  
       booksNotFinishedQuery = booksNotFinishedQuery.slice(0, -1) + ` AND CAST(bs.sequence as FLOAT) > (SELECT CAST(max(bs.sequence) as FLOAT) FROM bookSeries bs, mediaProgresses mp WHERE mp.mediaItemId = bs.bookId AND mp.isFinished = 1 AND mp.userId = :userId AND bs.seriesId = series.id)` + `)`
     }
 
